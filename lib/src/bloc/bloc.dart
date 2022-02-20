bff2b161e2c7edd8f845f3ec1131d15270d51494
blizzeratrops <bro.visser@gmail.com>
2022-02-20 16:15:58 -0300

    primer commit

---
+++ lib/src/bloc/bloc.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'package:login_bloc/src/bloc/validators.dart';
+
+class Bloc with Validators {
+  final _emailController = StreamController<String>();
+  final _passwordController = StreamController<String>();
+
+  Stream<String> get email => _emailController.stream.transform(validateEmail);
+  Stream<String> get password =>
+      _passwordController.stream.transform(validatePassword);
+
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
+}
